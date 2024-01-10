e1b55ff2dc49482a4b6eabfc1ea3e7c9ff35d8ab
DaviTheDinosaur <KongD@catlin.edu>
2024-01-09 19:04:49 -0800

    feat: driving, shooting and .gitignore

---
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import static frc.robot.Constants.LauncherConstants.*;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+public class Launcher extends SubsystemBase {
+  WPI_VictorSPX m_launchWheel;
+  WPI_VictorSPX m_feedWheel;
+
+  /** Creates a new Launcher. */
+  public Launcher() {
+    m_launchWheel = new WPI_VictorSPX(kLauncherID);
+    m_feedWheel = new WPI_VictorSPX(kFeederID);
+  }
+
+  /**
+   * This method is an example of the 'subsystem factory' style of command creation. A method inside
+   * the subsytem is created to return an instance of a command. This works for commands that
+   * operate on only that subsystem, a similar approach can be done in RobotContainer for commands
+   * that need to span subsystems. The Subsystem class has helper methods, such as the startEnd
+   * method used here, to create these commands.
+   */
+  public Command getIntakeCommand() {
+    // The startEnd helper method takes a method to call when the command is initialized and one to
+    // call when it ends
+    return this.startEnd(
+        // When the command is initialized, set the wheels to the intake speed values
+        () -> {
+          setFeedWheel(kIntakeFeederSpeed);
+          setLaunchWheel(kIntakeLauncherSpeed);
+        },
+        // When the command stops, stop the wheels
+            this::stop);
+  }
+
+  // An accessor method to set the speed (technically the output percentage) of the launch wheel
+  public void setLaunchWheel(double speed) {
+    m_launchWheel.set(speed);
+  }
+
+  // An accessor method to set the speed (technically the output percentage) of the feed wheel
+  public void setFeedWheel(double speed) {
+    m_feedWheel.set(speed);
+  }
+
+  // A helper method to stop both wheels.
+  public void stop() {
+    m_launchWheel.set(0);
+    m_feedWheel.set(0);
+  }
+}
